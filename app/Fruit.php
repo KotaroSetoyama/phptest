cd9a1979e1ca7b8967503646d5766db893971044
setoyama <kotaro.setoyama@gmail.com>
2020-05-16 18:20:14 +0900

    applied php-cs-fixer

--- app/Fruit.php
+++ app/Fruit.php
@@ -1,15 +1,17 @@
 <?php
-class Fruit {
-  // Properties
-  public $name;
-  public $color;
+class Fruit
+{
+    // Properties
+    public $name;
+    public $color;
 
-  // Methods
-  function set_name($name) {
-    $this->name = $name;
-  }
-  function get_name() {
-    return $this->name;
-  }
+    // Methods
+    public function set_name($name)
+    {
+        $this->name = $name;
+    }
+    public function get_name()
+    {
+        return $this->name;
+    }
 }
-?>
