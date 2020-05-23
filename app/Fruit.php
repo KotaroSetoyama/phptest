d48a4d90d471e73cc0c2f1d8d63dbe6b20e036e2
setoyama <kotaro.setoyama@gmail.com>
2020-05-23 22:52:38 +0900

    test

--- app/Fruit.php
+++ app/Fruit.php
@@ -3,17 +3,19 @@ class Fruit
 {
     // Properties
     public $name;
-        public $color;
+    public $color;
 
     // Methods
-    public function set_name($name){
+    public function set_name($name)
+    {
         $this->name = $name;
     }
 
 
 
 
-    public function get_name() {
+    public function get_name()
+    {
         return $this->name;
     }
 }
